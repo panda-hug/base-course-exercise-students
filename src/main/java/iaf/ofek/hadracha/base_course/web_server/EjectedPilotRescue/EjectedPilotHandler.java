1eb185c00ebd916212432c1bf44970532206f1b0
Lior Ramati <firerogue517@gmail.com>
2020-06-10 18:39:51 +0300

    Add stub for takeResponsiblity handler Also, IntelliJ deleted a bunch of random files??

--- src/main/java/iaf/ofek/hadracha/base_course/web_server/EjectedPilotRescue/EjectedPilotHandler.java
+++ src/main/java/iaf/ofek/hadracha/base_course/web_server/EjectedPilotRescue/EjectedPilotHandler.java
@@ -5,7 +5,9 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CookieValue;
 
 import java.util.List;
 
@@ -21,5 +23,12 @@ public class EjectedPilotHandler {
         @NotNull List<EjectedPilotInfo> pilotInfo = dataBase.getAllOfType(EjectedPilotInfo.class);
         // More logic?
         return pilotInfo;
+    }
+
+    @GetMapping("/takeResponsibility")
+    // TODO: Correct return type?
+    Object takeResponsibility(@CookieValue(name = "client-id") String clientId, @RequestParam int ejectionId) {
+        // all the logic
+        return null;
     }
 }
